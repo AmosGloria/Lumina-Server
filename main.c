a0dd5cb298a47fc9d110bf478f8f09e1f6e937ac
AmosGloria <amoskesegloria@gmail.com>
2026-04-19 22:19:39 +0000

    modified main.c file

--- main.c
+++ main.c
@@ -7,50 +7,74 @@
 
 #define PORT 8080
 
+char* read_file(const char* filename, long *size) {
+    FILE* f = fopen(filename, "rb");
+    if (!f) return NULL;
+
+    fseek(f, 0, SEEK_END);
+    *size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    char* string = malloc(*size + 1);
+    if (string) {
+        fread(string, *size, 1, f);
+        string[*size] = 0;
+    }
+    fclose(f);
+    return string;
+}
+
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
+    int opt = 1;
     int addrlen = sizeof(address);
     
-    // Raw HTTP response - this is the "infrastructure" layer
-    char *response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<h1>Lumina Server Online</h1><p>Running on C infrastructure.</p>";
-
-    // 1. Create socket (AF_INET = IPv4, SOCK_STREAM = TCP)
+    // Create socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket creation failed");
+        perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // 2. Set address and port
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // 3. Bind the socket to the port
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("Bind failed");
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    // 4. Start listening
     if (listen(server_fd, 10) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Lumina is waiting for a connection on port %d...\n", PORT);
+    printf("Lumina Professional Online at port %d\n", PORT);
 
     while(1) {
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
             perror("Accept failed");
-            exit(EXIT_FAILURE);
+            continue; // Don't kill the server, just wait for next client
         }
         
         char buffer[1024] = {0};
-        read(new_socket, buffer, 1024);
-        printf("Request Received:\n%s\n", buffer);
+        int valread = read(new_socket, buffer, 1024);
         
-        write(new_socket, response, strlen(response));
+        if (valread > 0) {
+            long html_size = 0;
+            char* html_content = read_file("index.html", &html_size);
+            
+            if (html_content) {
+                char *header = "HTTP/1.1 200 OK\nContent-Type: text/html\nConnection: close\n\n";
+                write(new_socket, header, strlen(header));
+                write(new_socket, html_content, html_size);
+                free(html_content);
+            }
+        }
         close(new_socket);
     }
     return 0;
